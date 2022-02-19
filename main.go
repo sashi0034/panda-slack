8a8631622c58e5808c8adb6f718d9f6036d31372
sashi0034 <satoshi3104.nishi@gmail.com>
2022-02-19 21:11:36 +0900

    特になし(修正)

--- main.go
+++ main.go
@@ -1,16 +1,41 @@
 package main
 
 import (
+	"encoding/json"
+	"log"
+	"os"
+
 	"github.com/slack-go/slack"
 )
 
+type configData struct {
+	BotToken    string `json:'botToken`
+	PostChannel string `json:postChannel`
+}
+
+func loadConfig() (*configData, error) {
+	f, err := os.Open("config.json")
+	if err != nil {
+		log.Fatal("loadConfig os.Open err:", err)
+		return nil, err
+	}
+	defer f.Close()
+
+	var cfg configData
+	err = json.NewDecoder(f).Decode(&cfg)
+	return &cfg, err
+}
+
+var config *configData
+
 func main() {
+	config, _ = loadConfig()
 	// アクセストークンを使用してクライアントを生成する
-	token := "xoxb-3069876617-3130854091778-fMxQjiXWMCIdLqclz5lON4aM"
+	token := config.BotToken
 	c := slack.New(token)
 
 	// MsgOptionText() の第二引数に true を設定すると特殊文字をエスケープする
-	_, _, err := c.PostMessage("C02BFCYV1DF", slack.MsgOptionText("Hello World\nわいわい", false))
+	_, _, err := c.PostMessage(config.PostChannel, slack.MsgOptionText("Hello World\nわいわい", false))
 	if err != nil {
 		panic(err)
 	}
